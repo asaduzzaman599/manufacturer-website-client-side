e0273ec2f4d65d1a342b548098af33fbdd2e871d
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-05-22 02:34:10 +0600

    react query added

--- src/Pages/Home/Parts.js
+++ src/Pages/Home/Parts.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import CommonTitle from '../../Components/CommonTitle';
 
 const Parts = () => {
+    const { isLoading, error, data } = useQuery('repoData', () =>
+        fetch('http://localhost:5000/product').then(res =>
+            res.json()
+        )
+    )
+
+    console.log(data)
     return (
         <div>
-            <CommonTitle>Vehicle Parts We Provide</CommonTitle>
+            <CommonTitle>Vehicle Parts We Provide: </CommonTitle>
         </div>
     );
 };
