ca7e26acba4f21f9d81374a0b419aeffcf9d1a70
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-05-25 04:14:54 +0600

    blog section added

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -3,11 +3,26 @@ import CommonHeading from '../Components/CommonHeading';
 
 const Blogs = () => {
     return (
-        <div>
+        <div className='my-6'>
             <CommonHeading>Blogs</CommonHeading>
-            <div className='w-4/5 mx-auto gaid gap-5'>
-                <div>
-                    <h3 className='text-2xl font-bold'></h3>
+            <div className='w-4/5 mx-auto gaid gap-8 text-left'>
+                <div className="shadow-lg p-6">
+                    <h3 className='text-xl font-bold'>How will you improve the performance of a React Application?</h3>
+                </div>
+                <div className="shadow-lg p-6">
+                    <h3 className='text-xl font-bold'>What are the different ways to manage a state in a React application?</h3>
+                </div>
+                <div className="shadow-lg p-6">
+                    <h3 className='text-xl font-bold'>How does prototypical inheritance work?</h3>
+                </div>
+                <div className="shadow-lg p-6">
+                    <h3 className='text-xl font-bold'>Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts</h3>
+                </div>
+                <div className="shadow-lg p-6">
+                    <h3 className='text-xl font-bold'>You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?</h3>
+                </div>
+                <div className="shadow-lg p-6">
+                    <h3 className='text-xl font-bold'>What is a unit test? Why should write unit tests?</h3>
                 </div>
             </div>
         </div>
