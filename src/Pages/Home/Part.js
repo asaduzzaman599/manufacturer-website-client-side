0512c4a9e716d92f4579edc70569e6c9c76b8902
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-05-26 15:42:16 +0600

    update align

--- src/Pages/Home/Part.js
+++ src/Pages/Home/Part.js
@@ -4,25 +4,27 @@ import { ShoppingCartIcon } from '@heroicons/react/solid'
 
 const Part = ({ part }) => {
     return (
-        <div className=" max-w-lg bg-base-100 shadow-xl hover:shadow-2xl mt-12 border-2" data-aos="fade-up"
+        <div className="flex flex-col justify-between max-w-lg bg-base-100 shadow-xl hover:shadow-2xl mt-12 border-2" data-aos="fade-up"
             data-aos-duration="3000">
-            <figure><img src={part?.img} className="w-full p-10" alt="vehicle part" /></figure>
-            <div className="card-body">
-                <h2 className="card-title font-bold  justify-center">
-                    <span>{part?.name}</span>
+            <div>
+                <figure><img src={part?.img} className="w-full p-10 h-60" alt="vehicle part" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title font-bold  justify-center">
+                        <span>{part?.name}</span>
 
-                </h2>
+                    </h2>
 
-                <hr />
-                <p className='text-center m-3 text-gray-400 text-sm'>{part?.description}</p>
-                <p className='text-2xl'><span className=' font-bold text-success'>${part?.price}</span> <small className='text-sm'>/per unit</small></p>
+                    <hr />
+                    <p className='text-center m-3 text-gray-400 text-sm'>{part?.description}</p>
+                    <p className='text-2xl'><span className=' font-bold text-success'>${part?.price}</span> <small className='text-sm'>/per unit</small></p>
 
-                <div className='text-right'>
+                    <div className='text-right'>
 
-                    <p><span className='font-bold'>Quantity:</span> {part?.quantity}</p>
-                    <p><span className='font-bold'>Minimum Order:</span> {part?.minimumOrder}</p>
+                        <p><span className='font-bold'>Quantity:</span> {part?.quantity}</p>
+                        <p><span className='font-bold'>Minimum Order:</span> {part?.minimumOrder}</p>
+                    </div>
+
                 </div>
-
             </div>
             <div className="card-actions w-full">
                 <Link to={`/order/${part?._id}`} className=" w-full  badge-info-content font-bold cursor-pointer py-4 hover:btn-primary duration-700 flex items-center justify-center gap-4">Purchase Now <ShoppingCartIcon className="h-5 w-5 " /></Link>
--- src/Pages/Protfolio/Education.js
+++ src/Pages/Protfolio/Education.js
@@ -3,8 +3,8 @@ import Institute from './Institute';
 
 const Education = () => {
     const institutes = [
-        { _id: 1, level: "School", institute: "Bhasantek School & College", duration: "2003 - 2014", descriptions: "Completed my School from Bhadantek school & College, Dhaka. I Passed ssc in 2014 with background science." },
-        { _id: 2, level: "College", institute: "Cambrian College", duration: "2014 - 2016", descriptions: "Completed my School from Cambrian Collage, Dhaka. I Passed HSC in 2016 with background science." },
+        { _id: 1, level: "School", institute: "Bhasantek School & College", duration: "2003 - 2014", descriptions: "Completed my School from Bhasantek school & College, Dhaka. I Passed ssc in 2014 with background science." },
+        { _id: 2, level: "College", institute: "Cambrian College", duration: "2014 - 2016", descriptions: "Completed my College from Cambrian Collage, Dhaka. I Passed HSC in 2016 with background science." },
         { _id: 3, level: "University", institute: "Independent University, Bangladesh", duration: "2017 - 2021", descriptions: "Completed my Graduation on CSE in from here in October, 2021" },
     ]
 
