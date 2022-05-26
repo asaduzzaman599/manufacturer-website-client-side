991c6f825124075d66b6908a70eb057e0e37575d
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-05-26 15:51:33 +0600

    update review

--- src/Pages/Shared/Reviewcard.js
+++ src/Pages/Shared/Reviewcard.js
@@ -8,13 +8,13 @@ const Reviewcard = ({ review, index }) => {
         <div className=" max-w-lg bg-base-100 shadow-xl mb-4 border-2" data-aos={index % 2 === 0 ? `fade-up-right` : `fade-up-left`} data-aos-duration="1200">
             <div className="card-body">
                 <div className='flex flex-col md:flex-row justify-between gap-4 items-center mb-3'>
-                    <div className='flex flex-col md:flex-row justify-start gap-2 items-center flex-1'>
+                    <div className='flex flex-col md:flex-row justify-start gap-4 items-center flex-1'>
                         <div className="avatar">
                             <div className="w-12 rounded-full ring ring-accent ring-offset-base-100 ">
                                 <img src={review.img || noUser} />
                             </div>
                         </div>
-                        <p className='text-secondary-content font-bold text-base flex-1'>{review.name}</p>
+                        <p className='text-secondary-content font-bold text-base md:text-left text-center flex-1'>{review.name}</p>
                     </div>
                     <div>
                         <RatingComponent review={review} />
